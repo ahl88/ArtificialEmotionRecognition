2976d00caf8f46655e37dc6214e1bd768494dbcd
Andy Lee <andy.huu.ly@gmail.com>
2020-02-19 00:54:58 -0500

    Added navigation bar

--- Test/src/main.js
+++ Test/src/main.js
@@ -1,3 +1,5 @@
+
+/*Collapses panels when clicked on sub header*/
 var coll = document.getElementsByClassName("collapse");
 var i;
 
@@ -12,3 +14,22 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+/*Hides and shows nav bar when scrolling*/
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (currentScrollPos < 10){
+    document.getElementById("navbar").style.top = "0";
+  } else{
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-80px";
+    }
+  }
+  prevScrollpos = currentScrollPos;
+
+}
+
+
