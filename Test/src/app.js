c9fd1c22f087e91f6e81ffe2a050b42077d0fce1
Andy Lee <ahl88@scarletmail.rutgers.edu>
2020-04-03 03:32:06 -0400

    Rearranged Web apps on the wesite  to feature each demo

---
+++ Test/src/app.js
@@ -0,0 +1,32 @@
+var express = require("express"),
+	app = express(),
+	http = require("http").Server(app).listen(80),
+	upload = require("express-fileupload");
+
+app.use(upload())
+app.use(express.json());
+app.use(express.static('../public'));
+
+
+console.log("Server Started!")
+
+app.get("/", function(req,res){
+	res.sendFile(__dirname+"/index.html")
+})
+
+app.post("/", function(req,res){
+	if(req.files){
+		var file = req.files.filename,
+			filename = req.files.filename.name
+		file.mv("./upload/" + filename,function(err){
+			if(err){
+				console.log(err)
+				res.send("error occured")
+			}
+			else{
+				res.send("Done!")
+			}
+		})
+
+	}
+})
